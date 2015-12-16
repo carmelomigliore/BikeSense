d3027f70db0ff184c94e6b94bcede7df68384254
Carmelo Migliore <car.miglio@gmail.com>
2015-12-16 16:09:58 +0100

    second commit

---
+++ BikeSense/.ino.cpp
@@ -0,0 +1,19 @@
+//This is a automatic generated file
+//Please do not modify this file
+//If you touch this file your change will be overwritten during the next build
+//This file has been generated on 2015-12-16 02:31:13
+
+#include "Arduino.h"
+#include <Arduino.h>
+#include "Adafruit_BLE.h"
+#include "Adafruit_BluefruitLE_SPI.h"
+#include "BluefruitConfig.h"
+#include "Adafruit_FONA.h"
+#include "LowPower.h"
+#include <avr/wdt.h>
+void error(const __FlashStringHelper*err) ;
+void setup() ;
+void loop() ;
+
+#include "BikeSense.ino"
+
