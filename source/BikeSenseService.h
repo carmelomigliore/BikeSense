f62f36cb8b2acfca53ec2c8957edb5880f19b5a3
Carmelo Migliore <car.miglio@gmail.com>
2016-03-09 11:20:26 +0100

    Coordinates sent by board instead of beeing server-side simulated (demo)

---
+++ source/BikeSenseService.h
@@ -0,0 +1,91 @@
+/* mbed Microcontroller Library
+ * Copyright (c) 2006-2013 ARM Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef __BLE_BIKESENSE_SERVICE_H__
+#define __BLE_BIKESENSE_SERVICE_H__
+#include "mbed-drivers/mbed.h"
+
+
+class BikeSenseService {
+public:
+    const static uint16_t BIKESENSE_SERVICE_UUID = 0xA000;
+    const static uint16_t GAS_VALUE_CHARACTERISTIC_UUID = 0xA001;
+    const static uint16_t TEMP_VALUE_CHARACTERISTIC_UUID = 0xA002;
+    const static uint16_t HUMIDITY_VALUE_CHARACTERISTIC_UUID = 0xA003;
+    const static uint16_t BATTERY_VALUE_CHARACTERISTIC_UUID = 0xA004;
+    const static uint16_t COMMAND_CHARACTERISTIC_UUID = 0xA005;
+    const static uint16_t POTHOLE_CHARACTERISTIC_UUID = 0xA006;
+    const static uint16_t ANTITHEFT_CHARACTERISTIC_UUID = 0xA007;
+
+    BikeSenseService(BLEDevice &_ble, uint16_t initialValueForGASCharacteristic, int16_t initialValueForTEMPCharacteristic, uint16_t initialValueForHUMIDITYCharacteristic, 
+		uint16_t initialValueForBATTERYCharacteristic, uint16_t initialValueForCOMMANDCharacteristic, uint8_t initialValueForPOTHOLECharacteristic, uint8_t initialValueForANTITHEFTCharacteristic) :
+        ble(_ble), gasValue(GAS_VALUE_CHARACTERISTIC_UUID, &initialValueForGASCharacteristic), tempValue(TEMP_VALUE_CHARACTERISTIC_UUID, &initialValueForTEMPCharacteristic),
+	humidityValue(HUMIDITY_VALUE_CHARACTERISTIC_UUID, &initialValueForHUMIDITYCharacteristic), batteryValue(BATTERY_VALUE_CHARACTERISTIC_UUID, &initialValueForBATTERYCharacteristic),
+	command(COMMAND_CHARACTERISTIC_UUID, &initialValueForCOMMANDCharacteristic), pothole(POTHOLE_CHARACTERISTIC_UUID, &initialValueForPOTHOLECharacteristic, GattCharacteristic::BLE_GATT_CHAR_PROPERTIES_NOTIFY), 
+	antitheft(ANTITHEFT_CHARACTERISTIC_UUID ,&initialValueForANTITHEFTCharacteristic, GattCharacteristic::BLE_GATT_CHAR_PROPERTIES_NOTIFY)
+    {
+        GattCharacteristic *charTable[] = {&gasValue, &tempValue, &humidityValue, &batteryValue, &command, &pothole, &antitheft};
+        GattService         bikeSenseService(BIKESENSE_SERVICE_UUID, charTable, sizeof(charTable) / sizeof(GattCharacteristic *));
+        ble.addService(bikeSenseService);
+    }
+
+    GattAttribute::Handle_t getCommandHandle() const {
+        return command.getValueHandle();
+    }
+
+    void updateGasValue(uint16_t newValue) {
+        ble.gattServer().write(gasValue.getValueHandle(), 
+            (uint8_t *)&newValue, sizeof(uint16_t));
+    }
+
+    void updateTempValue(int16_t newValue) {
+        ble.gattServer().write(tempValue.getValueHandle(), 
+            (uint8_t *)&newValue, sizeof(int16_t));
+    }
+
+    void updateHumidityValue(uint16_t newValue) {
+        ble.gattServer().write(humidityValue.getValueHandle(), 
+            (uint8_t *)&newValue, sizeof(uint16_t));
+    }
+
+    void updateBatteryValue(uint16_t newValue) {
+        ble.gattServer().write(batteryValue.getValueHandle(), 
+            (uint8_t *)&newValue, sizeof(uint16_t));
+    }
+    
+    void updatePotholeValue(uint8_t newValue) {
+        ble.updateCharacteristicValue(pothole.getValueHandle(), 
+            (uint8_t *)&newValue, sizeof(uint8_t));
+    }
+    
+    void updateAntitheftValue(uint8_t newValue) {
+        ble.updateCharacteristicValue(antitheft.getValueHandle(), 
+            (uint8_t *)&newValue, sizeof(uint8_t));
+    }
+    
+
+private:
+    BLEDevice                         &ble;
+    ReadOnlyGattCharacteristic<uint16_t>  gasValue;
+    ReadOnlyGattCharacteristic<int16_t>  tempValue;
+    ReadOnlyGattCharacteristic<uint16_t>  humidityValue;
+    ReadOnlyGattCharacteristic<uint16_t>  batteryValue;
+    ReadWriteGattCharacteristic<uint16_t>  command;
+    ReadWriteGattCharacteristic<uint8_t> pothole;
+    ReadOnlyGattCharacteristic<uint8_t> antitheft;
+};
+
+#endif /* #ifndef __BLE_BIKESENSE_SERVICE_H__ */
--- source/app.cpp
+++ source/app.cpp
@@ -4,25 +4,206 @@
 #include "sockets/v0/UDPSocket.h"
 #include "sal-stack-lwip/lwipv4_init.h"
 #include "ble/BLE.h"
-#include "ble/services/iBeacon.h"
+//#include "ble/services/UARTService.h"
+#include "BikeSenseService.h"
+#include "MMA8451Q.h"
 
 // 
+//#ifndef DEBUG
+//#define DEBUG
+//#endif
+
+#ifdef DEBUGPRINT
+#define DEBUG_PRINT(fmt, args...)    printf(fmt, ## args)
+#else
+#define DEBUG_PRINT(fmt, args...)    /* Don't do anything in release builds */
+#endif
+
+const static char     DEVICE_NAME[] = "BikeSense";
+static const uint16_t uuid16_list[] = {BikeSenseService::BIKESENSE_SERVICE_UUID/*, UARTServiceShortUUID*/};
+BikeSenseService *bikeSenseServicePtr;
+//UARTService *uartServicePtr;
+//BLEDevice   ble;
+
+static FONA808 myFona(D8,D2,D12);
+static MMA8451Q acc(D14,D15,PA_0,0x1d<<1);
+
+int x=100,y=100,z=100;
+bool flag = false;
+uint8_t anti = 0;
+int posindex = 0;
+
+float positions[][2] = {{7.662717103958131,45.062343893659175},{7.663382291793824,45.063329047712855},{7.66398310661316,45.06413989254349},{7.664508819580079,45.06488252538225},{7.664937973022462,45.065511482384075},{7.665388584136964,45.06614043246582},{7.665785551071168,45.06668602211236},{7.666150331497193,45.06723160655154},{7.665538787841798,45.06749681932851},{7.664905786514283,45.067724143586574},{7.66424059867859,45.06797419922623},{7.663865089416505,45.067595326618004},{7.663575410842896,45.06720129644151},{7.664090394973756,45.067049745650394}};
+
+float longitude = 7.662717103958131;
+float latitude = 45.062343893659175;
+
+static void clearFlag(){
+	flag = false;
+}
+
+static void setPosition(){
+	latitude=positions[posindex][1];
+	longitude=positions[posindex][0];
+	if(++posindex==14){
+		posindex=0;
+	}
+} 
 
 using namespace mbed::Sockets::v0;
+
+class Connection { 
+private:
+	TCPStream _sock;
+	uint16_t* send;
+	bool connected;
+	SocketAddr sockaddr;
+
+public:
+	Connection(): _sock(SOCKET_STACK_LWIP_IPV4), connected(false){
+		_sock.open(SOCKET_AF_INET4);
+		_sock.setNagle(false);
+		 send = new uint16_t[16];
+      		 for(int i =0; i< 16; i++){
+	  	 send[i] = i;
+		}
+	}
+	void onReceive(Socket *s) {
+        //DEBUG_PRINT("\nOnReceive");
+	uint16_t result[64];
+	size_t size = 20;
+	socket_error_t err = s->recv(result, &size);
+        DEBUG_PRINT("MBED: Socket Error: %s (%d)\r\n", socket_strerror(err), err);
+	DEBUG_PRINT("\nThe result is %f",result[0]);
+	send[0] = rand();
+        send[1] = rand();
+        send[2] = rand();
+        socket_error_t err2 = _sock.send(send, 16*sizeof(float));
+        DEBUG_PRINT("MBED: Socket Error: %s (%d)\r\n", socket_strerror(err2), err2);
+    }
+
+   
+    void onConnect(TCPStream *s) {
+    	connected=true;
+  	//DEBUG_PRINT("\nConnected!"); 
+        s->setOnReadable(TCPStream::ReadableHandler_t(this, &Connection::onReceive));
+        s->setOnDisconnect(TCPStream::DisconnectHandler_t(this, &Connection::onDisconnect));
+        mbed::util::FunctionPointer0<void> ptr(this,&Connection::sendPosition);
+         minar::Scheduler::postCallback(ptr.bind()).period(minar::milliseconds(4000));
+
+         minar::Scheduler::postCallback(setPosition).period(minar::milliseconds(4000));
+        /*send[0] = 666;
+	       // send[1] = acc.getAccY();
+		//send[2] = acc.getAccZ();
+		socket_error_t err = _sock.send(send, 16*sizeof(uint16_t));
+		DEBUG_PRINT("MBED: Socket Error: %s (%d)\r\n", socket_strerror(err), err);
+		s->error_check(err);*/
+  }
+  
+  void sendHole(){
+  	       send[0] = 666;
+  	       float* aux = (float*)&(send[1]);
+  	       aux[0] = latitude;
+  	       aux[1] = longitude;
+	       // send[1] = acc.getAccY();
+		//send[2] = acc.getAccZ();
+		socket_error_t err = _sock.send(send, 16*sizeof(uint16_t));
+		DEBUG_PRINT("MBED: Socket Error: %s (%d)\r\n", socket_strerror(err), err);
+		//s->error_check(err);
+  }
+  
+  void sendAlarm(){
+  	send[0] = 668;
+       // send[1] = acc.getAccY();
+	//send[2] = acc.getAccZ();
+	socket_error_t err = _sock.send(send, 16*sizeof(uint16_t));
+	DEBUG_PRINT("MBED: Socket Error: %s (%d)\r\n", socket_strerror(err), err);
+	//s->error_check(err);
+  }
+  
+  void sendPosition(){
+  	if(!anti){
+	  	DEBUG_PRINT("Sending position\n");
+	  	 send[0] = 665;
+	  	       float* aux = (float*)&(send[1]);
+	  	       aux[0] = latitude;
+	  	       aux[1] = longitude;
+	  		socket_error_t err = _sock.send(send, 16*sizeof(uint16_t));
+			DEBUG_PRINT("MBED: Socket Error: %s (%d)\r\n", socket_strerror(err), err);
+		}
+  }
+  
+   bool isConnected(){
+   	return connected;
+   }
+
+   void onDisconnect(TCPStream *s) {
+        s->close();
+        connected=false;
+       // DEBUG_PRINT("{{%s}}\r\n",(error()?"failure":"success"));
+        //DEBUG_PRINT("{{end}}\r\n");
+    }
+
+    socket_error_t connect(SocketAddr sa) {
+    		
+    		sockaddr = sa;
+      		socket_error_t err = _sock.connect(sa, 25, TCPStream::ConnectHandler_t(this, &Connection::onConnect));
+		//return _sock.resolve(address,UDPSocket::DNSHandler_t(this, &Resolver::onDNS));
+		//DEBUG_PRINT("\nerr: %d",err);
+		DEBUG_PRINT("MBED: Socket Error: %s (%d)\r\n", socket_strerror(err), err);
+		
+    }
+    
+    socket_error_t connect() {
+    	connect(sockaddr);
+    }
+    
+    void disconnect(){
+    	printf("Disonnect\n");
+    	_sock.close();
+    }
+    
+
+};
+
+static Connection *conn;
+
+
+static void readMma(){
+	DEBUG_PRINT("\nReadMMA\n");
+	uint16_t send[40];
+	int tempx, tempy, tempz; 
+	//acc.registerDump();
+	tempx = 100*acc.getAccX();
+	tempy = 100*acc.getAccY();
+	tempz = 100*acc.getAccZ();
+	DEBUG_PRINT("\nReadMMA2\n");	
+	if((tempx - x > 50 || tempx - x < -50 || tempy-y > 50 || tempy -y < -50) && !flag){
+		flag = true;
+		conn->sendHole();
+		bikeSenseServicePtr->updatePotholeValue(1);
+		printf("Buca!\n");
+		minar::Scheduler::postCallback(clearFlag).delay(minar::milliseconds(3000));
+	}
+	x = tempx;
+	y = tempy;
+	z = tempz;
+	printf("X: %d\n", x);
+	/*printf("Y: %d\n", y);
+	printf("Z: %d\n", z);*/
+	
+}
+
+
 class Resolver {
 private:
-    TCPStream _sock;
     UDPSocket _dns;
-    uint16_t* send;
+    Connection *myconn;
 public:
-    Resolver() : _sock(SOCKET_STACK_LWIP_IPV4), _dns(SOCKET_STACK_LWIP_IPV4) {
-        _sock.open(SOCKET_AF_INET4);
-	_sock.setNagle(false);
+    Resolver(Connection *conn) : _dns(SOCKET_STACK_LWIP_IPV4) {
+        
  	_dns.open(SOCKET_AF_INET4);
-       send = new uint16_t[64];
-       for(int i =0; i< 64; i++){
-	   send[i] = i;
-	}
+ 	myconn= conn;
     }
 
     void onDNS(Socket *s, struct socket_addr addr, const char *domain) {
@@ -31,68 +212,49 @@ public:
         char buf[16];
         sa.setAddr(&addr);
         sa.fmtIPv4(buf,sizeof(buf));
-        printf("Resolved %s to %s\r\n", domain, buf);
-        socket_error_t err = _sock.connect(sa, 25, TCPStream::ConnectHandler_t(this, &Resolver::onConnect));
-        //return _sock.resolve(address,UDPSocket::DNSHandler_t(this, &Resolver::onDNS));
-        //printf("\nerr: %d",err);
-        printf("MBED: Socket Error: %s (%d)\r\n", socket_strerror(err), err);
+        DEBUG_PRINT("Resolved %s to %s\r\n", domain, buf);
+        myconn->connect(sa);
+        minar::Scheduler::postCallback(readMma).period(minar::milliseconds(200));
+        
+        
     }
 
-    void onReceive(Socket *s) {
-        printf("\nOnReceive");
-	uint16_t result[64];
-	size_t size = 20;
-	socket_error_t err = s->recv(result, &size);
-        printf("MBED: Socket Error: %s (%d)\r\n", socket_strerror(err), err);
-	printf("\nThe result is %d",result[0]);
-    }
-
-   
-    void onConnect(TCPStream *s) {
-  	printf("\nConnected!"); 
-        s->setOnReadable(TCPStream::ReadableHandler_t(this, &Resolver::onReceive));
-        s->setOnDisconnect(TCPStream::DisconnectHandler_t(this, &Resolver::onDisconnect));
-        send[0] = 80;
-        send[1] = 480;
-        socket_error_t err = _sock.send(send, 64*sizeof(uint16_t));
-        printf("MBED: Socket Error: %s (%d)\r\n", socket_strerror(err), err);
-        //s->error_check(err);
-  }
-
-   void onDisconnect(TCPStream *s) {
-        s->close();
-       // printf("{{%s}}\r\n",(error()?"failure":"success"));
-        printf("{{end}}\r\n");
-    }
-
-    socket_error_t connect(const char * address) {
-        /*SocketAddr ipaddr;
-        ipaddr.setAddr(SOCKET_AF_INET4, "52.29.108.135");
-        printf("Connecting to %s...\r\n", address);
-        socket_error_t err = _sock.connect(ipaddr, 25, TCPStream::ConnectHandler_t(this, &Resolver::onConnect));
-        //return _sock.resolve(address,UDPSocket::DNSHandler_t(this, &Resolver::onDNS));
-        //printf("\nerr: %d",err);
-        printf("MBED: Socket Error: %s (%d)\r\n", socket_strerror(err), err);*/
-    }
+    
    
     socket_error_t resolve(const char * address) {
-        printf("Resolving %s...\r\n", address);
+        //DEBUG_PRINT("Resolving %s...\r\n", address);
         return _dns.resolve(address,UDPSocket::DNSHandler_t(this, &Resolver::onDNS));
     }
 };
 
+static Resolver *r;
 
 static void blinky(void) {
     static DigitalOut led(LED1);
     led=!led;
 }
 
-//static void dummy(void){
-//}
+static void setAnti(){
+	bikeSenseServicePtr->updateAntitheftValue(!anti);
+	anti = !anti;
+}
+
+static void dummy(void){
+	//printf("Testing connection");
+	//conn->disconnect();
+	//conn->connect();
+	mbed::util::FunctionPointer0<void> ptr(conn,&Connection::sendAlarm);
+        minar::Scheduler::postCallback(ptr.bind());
+	//conn->sendAlarm();
+	minar::Scheduler::postCallback(setAnti);
+		
+}
+
+static InterruptIn butt(USER_BUTTON);
+
 
 /*static void enterSTOP(void){
-  static InterruptIn butt(USER_BUTTON);
-  butt.fall(dummy);
+  
   /* User push-button (External line 13) will be used to wakeup the system from STOP mode */
   //BSP_PB_Init(BUTTON_USER, BUTTON_MODE_EXTI);
 
@@ -102,11 +264,11 @@ static void blinky(void) {
   /* Ensure that MSI is wake-up system clock */ 
 /*  __HAL_RCC_WAKEUPSTOP_CLK_CONFIG(RCC_STOP_WAKEUPCLOCK_MSI);
 
-   printf("Entering stop mode\n");
+   DEBUG_PRINT("Entering stop mode\n");
    HAL_Delay(3000);
    HAL_PWR_EnterSTOPMode(PWR_LOWPOWERREGULATOR_ON, PWR_STOPENTRY_WFI);
    SetSysClock();
-   printf("Woken up!\n");
+   DEBUG_PRINT("Woken up!\n");
 }*/
 //static RawSerial fona(D8,D2);
 
@@ -114,20 +276,19 @@ static void handler(void){
    // putc(fona.getc(),stdout);
 }
 
-static Resolver *r;
-
 void resolve_dns(){
- printf("\nResolve DNS");
+//butt.fall(dummy);
+ //DEBUG_PRINT("\nResolve DNS");
  lwipv4_socket_init();
- r = new Resolver();
+
+ conn = new Connection();
+  r = new Resolver(conn);
  //r->connect("mbed.org");
  r->resolve("ec2-52-29-108-135.eu-central-1.compute.amazonaws.com");
-/* r->connect("mbed.org");
+ /* r->connect("mbed.org");
  r->connect("mbed.org");*/
 }
 
-
-static iBeacon* ibeaconPtr;
 
 /**
  * This function is called when the ble initialization process has failled
@@ -135,15 +296,27 @@ static iBeacon* ibeaconPtr;
 void onBleInitError(BLE &ble, ble_error_t error)
 {
     /* Initialization error handling should go here */
-     printf("\nEzechiele 25-17");
+     DEBUG_PRINT("\nEzechiele 25-17");
 }
+
+void disconnectionCallback(const Gap::DisconnectionCallbackParams_t *params)
+{
+      BLE::Instance().gap().startAdvertising();
+}
+
+
+void onDataWrittenCallback(const GattWriteCallbackParams *params) {
+        if ((params->handle == bikeSenseServicePtr->getCommandHandle())) {
+        	DEBUG_PRINT("OnDataWritten\n");
+        }
+    }
 
 /**
  * Callback triggered when the ble initialization process has finished
  */
 void bleInitComplete(BLE::InitializationCompleteCallbackContext *params)
 {
-    printf("\nBLE init complete");
+    DEBUG_PRINT("\nBLE init complete");
     BLE&        ble   = params->ble;
     ble_error_t error = params->error;
 
@@ -158,45 +331,102 @@ void bleInitComplete(BLE::InitializationCompleteCallbackContext *params)
         return;
     }
 
-    /**
-     * The Beacon payload has the following composition:
-     * 128-Bit / 16byte UUID = E2 0A 39 F4 73 F5 4B C4 A1 2F 17 D1 AD 07 A9 61
-     * Major/Minor  = 0x1122 / 0x3344
-     * Tx Power     = 0xC8 = 200, 2's compliment is 256-200 = (-56dB)
-     *
-     * Note: please remember to calibrate your beacons TX Power for more accurate results.
-     */
-    static const uint8_t uuid[] = {0xE2, 0x0A, 0x39, 0xF4, 0x73, 0xF5, 0x4B, 0xC4,
-                                   0xA1, 0x2F, 0x17, 0xD1, 0xAD, 0x07, 0xA9, 0x61};
-    uint16_t majorNumber = 1122;
-    uint16_t minorNumber = 3344;
-    uint16_t txPower     = 0xC8;
-    ibeaconPtr = new iBeacon(ble, uuid, majorNumber, minorNumber, txPower);
+    ble.gap().onDisconnection(disconnectionCallback);
+    ble.gattServer().onDataWritten(onDataWrittenCallback);
 
-    ble.gap().setAdvertisingInterval(1000); /* 1000ms. */
-    ble.gap().startAdvertising();
+	
+	bikeSenseServicePtr = new BikeSenseService (ble, 0,2,0,4,0,0,0 /* initial values */);
+	//uartServicePtr = new UARTService(ble);
+
+	/* setup advertising */
+	ble.gap().accumulateAdvertisingPayload(GapAdvertisingData::BREDR_NOT_SUPPORTED
+	     | GapAdvertisingData::LE_GENERAL_DISCOVERABLE);
+	ble.gap().accumulateAdvertisingPayload(GapAdvertisingData::
+	    COMPLETE_LIST_16BIT_SERVICE_IDS, 
+	    (uint8_t *)uuid16_list, sizeof(uuid16_list));
+	ble.gap().accumulateAdvertisingPayload(GapAdvertisingData::
+	    COMPLETE_LOCAL_NAME, 
+	    (uint8_t *)DEVICE_NAME, sizeof(DEVICE_NAME));
+	ble.gap().setAdvertisingType(GapAdvertisingParams::
+	    ADV_CONNECTABLE_UNDIRECTED);
+	/* 1000ms. */
+	ble.gap().setAdvertisingInterval(1000);
+	ble.gap().startAdvertising();
 }
-
-
-static FONA808 myFona(D8,D2,D12);
 
 
 static void fona_prova(void){
-  printf("Hello!\n");
  //myFona.init();
- myFona.connect("ibox.tim.it",NULL,NULL);
+ int ret;
+ do{
+ 	ret = myFona.connect("ibox.tim.it",NULL,NULL);
+ } while(ret!=0);
  minar::Scheduler::postCallback(resolve_dns).delay(minar::milliseconds(8000));
- 
+}
+
+/*static void periodicRead(){
+	int c;
+	while((c = uartServicePtr->_getc()) != EOF){
+		DEBUG_PRINT("%c",(char)c);
+	}
+        fflush(stdout);
+	uartServicePtr->writeString("Birra\n");
+}*/
+
+/*static I2C i2c(D14,D15);
+static InterruptIn i1(D11);
+static InterruptIn i2(D10);*/
+
+
+static void mmaInit(){
+	uint8_t ret = acc.getWhoAmI();
+	DEBUG_PRINT("Ret: %d\n",ret);
+	//acc.setPotholeInterrupt();
+	DEBUG_PRINT("Pothole interrupt set\n");
+	//float x = acc.getAccX();
+	//printf("X: %f\n", x);
+	/*printf("Accelerometer start\n");
+	if(!acc.begin(0x1C)){
+		printf("Birrozza\n");
+	}
+	else{
+		printf("Birretta\n");
+	}
+	printf("Accelerometer begin\n");
+	acc.setRange(MMA8451_RANGE_2_G);
+	printf("Accelerometer range set\n");
+	printf("Range: %d\n",2 << acc.getRange());*/
+	
 }
 
 
+/*static void i2csearch(){
+	printf("I2CU! Searching for I2C devices...\n");
+   	int count = 0;
+    	for (int address=0; address<256; address+=2) {
+        	if (!i2c.write(address, NULL, 0)) { // 0 returned is ok
+        	    printf(" - I2C device found at address 0x%02X\n", address);
+        	    count++;
+        	}
+    	}	
+    	printf("%d devices found\n", count);
+}*/
+
+
+
 void app_start(int, char**) {
-   minar::Scheduler::postCallback(blinky).period(minar::milliseconds(1000));
+   butt.fall(dummy);
+    minar::Scheduler::postCallback(blinky).period(minar::milliseconds(1000));
+    //minar::Scheduler::postCallback(readMma).period(minar::milliseconds(300)).delay(minar::milliseconds(2000));
     BLE &ble = BLE::Instance();
     ble.init(bleInitComplete);
+    minar::Scheduler::postCallback(fona_prova).delay(minar::milliseconds(2000));
+    DEBUG_PRINT("Hello!\n");
+    //minar::Scheduler::postCallback(mmaInit);
+    
    // minar::Scheduler::postCallback(enterSTOP).delay(minar::milliseconds(10000));
-   //minar::Scheduler::postCallback(fona_prova);
-
+   
+   // minar::Scheduler::postCallback(periodicRead).delay(minar::milliseconds(4000)).period(minar::milliseconds(10000));
 }
 
 
